2c8a88fa648dd1adb838da096585274a89e6c5dc
Anikett77 <aniketchaturvedi83@gmail.com>
2026-05-24 16:27:14 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, redirect, send_from_directory, jsonify
+from flask_cors import CORS
+import os
+import numpy as np
+import json
+import uuid
+import tensorflow as tf
+import gdown
+
+app = Flask(__name__)
+CORS(app)
+
+# Download model from Google Drive if not present
+MODEL_PATH = "plant_disease_recogn_model_pwp.keras"
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+    gdown.download(
+        "https://drive.google.com/file/d/1URKojLNk1IE_ch-tXj4wdNPO4ylV_mTB/view?usp=sharing",
+        MODEL_PATH,
+        quiet=False
+    )
+
+model = tf.keras.models.load_model(MODEL_PATH)
+label = ['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Background_without_leaves',
+ 'Blueberry___healthy',
+ 'Cherry___Powdery_mildew',
+ 'Cherry___healthy',
+ 'Corn___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn___Common_rust',
+ 'Corn___Northern_Leaf_Blight',
+ 'Corn___healthy',
+ 'Grape___Black_rot',
+ 'Grape___Esca_(Black_Measles)',
+ 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+ 'Grape___healthy',
+ 'Orange___Haunglongbing_(Citrus_greening)',
+ 'Peach___Bacterial_spot',
+ 'Peach___healthy',
+ 'Pepper,_bell___Bacterial_spot',
+ 'Pepper,_bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Raspberry___healthy',
+ 'Soybean___healthy',
+ 'Squash___Powdery_mildew',
+ 'Strawberry___Leaf_scorch',
+ 'Strawberry___healthy',
+ 'Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
+
+with open("plant_disease.json",'r') as file:
+    plant_disease = json.load(file)
+
+# print(plant_disease[4])
+
+@app.route('/uploadimages/<path:filename>')
+def uploaded_images(filename):
+    return send_from_directory('./uploadimages', filename)
+
+@app.route('/',methods = ['GET'])
+def home():
+    return render_template('home.html')
+
+def extract_features(image):
+    image = tf.keras.utils.load_img(image,target_size=(160,160))
+    feature = tf.keras.utils.img_to_array(image)
+    feature = np.array([feature])
+    return feature
+
+def model_predict(image):
+    img = extract_features(image)
+    prediction = model.predict(img)
+    # print(prediction)
+    prediction_label = plant_disease[prediction.argmax()]
+    return prediction_label
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    image = request.files['img']
+    os.makedirs('uploadimages', exist_ok=True)
+    filename = f"temp_{uuid.uuid4().hex}_{image.filename}"
+    filepath = os.path.join('uploadimages', filename)
+    image.save(filepath)
+    prediction = model_predict(filepath)
+    return jsonify({
+        "prediction": prediction,
+        "image_url": f"http://127.0.0.1:5000/uploadimages/{filename}"
+    })
+        
+    
+if __name__ == "__main__":
+    app.run(debug=True)
